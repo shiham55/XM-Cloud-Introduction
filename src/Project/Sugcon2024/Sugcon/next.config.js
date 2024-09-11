f04d1e312eb58dc341618e87dcf6f11b8146bfc8
Shiham <shiham55@gmail.com>
2024-09-11 18:22:02 +0800

    Update next.config.js
    reverted the change

--- src/Project/Sugcon2024/Sugcon/next.config.js
+++ src/Project/Sugcon2024/Sugcon/next.config.js
@@ -9,7 +9,7 @@ const publicUrl = getPublicUrl();
  */
 const nextConfig = {
   // Allow specifying a distinct distDir when concurrently running app in a container
-  // distDir: process.env.NEXTJS_DIST_DIR || '.next',
+  distDir: process.env.NEXTJS_DIST_DIR || '.next',
 
   // Make the same PUBLIC_URL available as an environment variable on the client bundle
   env: {
